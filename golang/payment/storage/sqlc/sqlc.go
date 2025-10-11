d459a7ca5e58e4b4681b2cd177a995ac2e925c75
m4gshm <mfourgeneralsherman@gmail.com>
2025-10-12 00:02:43 +0300

    fix: build

--- golang/payment/storage/sqlc/sqlc.go
+++ golang/payment/storage/sqlc/sqlc.go
@@ -1,3 +1,5 @@
 package sqlc
 
 //go:generate sqlc generate -f ./sqlc.yaml
+//go:generate sqlc vet
+
--- golang/reserve/storage/sqlc/sqlc.go
+++ golang/reserve/storage/sqlc/sqlc.go
@@ -2,3 +2,4 @@ package sqlc
 
 //go:generate sqlc generate -f ./sqlc.yaml
 //go:generate sqlc vet
+
