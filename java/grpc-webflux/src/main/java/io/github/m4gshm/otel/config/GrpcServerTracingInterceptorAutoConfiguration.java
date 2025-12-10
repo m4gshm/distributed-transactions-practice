d5821647ae3aeb4503822b19d22b10637e3dc13b
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2025-12-11 02:18:39 +0300

    java tracing

--- java/grpc-webflux/src/main/java/io/github/m4gshm/otel/config/GrpcServerTracingInterceptorAutoConfiguration.java
+++ java/grpc-webflux/src/main/java/io/github/m4gshm/otel/config/GrpcServerTracingInterceptorAutoConfiguration.java
@@ -2,11 +2,12 @@ package io.github.m4gshm.otel.config;
 
 import io.grpc.ServerInterceptor;
 import io.opentelemetry.instrumentation.grpc.v1_6.GrpcTelemetry;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 
-@AutoConfiguration(after = GrpcTelemetryAutoConfiguration.class)
+@AutoConfiguration(after = { ObservationAutoConfiguration.class, GrpcTelemetryAutoConfiguration.class })
 public class GrpcServerTracingInterceptorAutoConfiguration {
 
     @Bean
